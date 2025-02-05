70a320af040df7187422ac717eb43ffaa964d3f0
vanix056 <abdullahwaqar121105@gmail.com>
2025-02-06 03:27:58 +0500

    Read_data

--- src/app.py
+++ src/app.py
@@ -6,3 +6,19 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from util import read_data,preprocess_data,model_train,evaluation
+
+working_dir=os.path.dirname(os.path.abspath(__file__))
+parent_dir=os.path.dirname(working_dir)
+
+
+st.set_page_config(title="No Code ML",icon="🧠",layout="centered")
+st.title("No Code ML Training")
+dataset_list=os.listdir(f'{parent_dir}/data')
+
+dataset=st.selectbox("Select the dataset to train",dataset_list,index=None)
+
+
+df = read_data(dataset)
+
+
+
--- src/util.py
+++ src/util.py
@@ -9,3 +9,24 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
+
+working_dir=os.path.dirname(os.path.abspath(__file__))
+parent_dir=os.path.dirname(working_dir)
+
+
+def read_data(dataset):
+    data_path = f'{parent_dir}/data/{dataset}'
+    if data_path.endswith('.csv'):
+        df = pd.read_csv(data_path)
+        return df
+    elif data_path.endswith('.xlsx') or data_path.endswith('xls'):
+        df = pd.read_excel(data_path)
+        return df
+        
+    
+def preprocess_data():
+    
+def model_train():
+    
+def evaluation():
+
