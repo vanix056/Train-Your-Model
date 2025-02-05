adf4faca9e78a1684566d0efb7d6725a4fda6cdb
vanix056 <abdullahwaqar121105@gmail.com>
2025-02-06 03:12:50 +0500

    2nd done

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import os
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
